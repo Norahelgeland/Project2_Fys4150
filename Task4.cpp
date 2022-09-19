8072c437634b28c494b3698c9fb51c2d813b3056
NoraHelgeland <norahelgeland@gmail.no>
2022-09-19 12:50:27 +0200

    Second commit

--- Task4.cpp
+++ Task4.cpp
@@ -13,7 +13,7 @@ void jacobi_eigensolver(const arma::mat& A, double eps, arma::vec& eigenvalues, 
 int main(){
 
     arma::mat A = arma::mat();
-    A.load("fileT");
+    A.load("file4");
     arma::vec eigenvalues;
     arma::mat eigenvectors;
     int iterations;
@@ -22,10 +22,10 @@ int main(){
 
     int k;
     int l;
+    //max_offdiag_symmetric(A, k, l);
+    jacobi_eigensolver(A, pow(10,-20), eigenvalues, eigenvectors, maxiter, iterations, converged);
+    //arma::mat R = arma::mat(A.n_rows, A.n_cols, arma::fill::eye);
 
-    // jacobi_eigensolver(A, pow(10,8), eigenvalues, eigenvectors, maxiter, iterations, converged);
-    arma::mat R = arma::mat(A.n_rows, A.n_cols, arma::fill::eye);
-    jacobi_rotate(A, R, k, l);
 
 }
 
@@ -36,7 +36,7 @@ int main(){
 // - Modifies the input matrices A and R
 void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
 
-    double tau = (A(l,l) - A(k,k))/(2*A(k,k));
+    double tau = (A(l,l) - A(k,k))/(2*A(k,l));
     
     double tan = 1/(tau+sqrt(1+pow(tau,2)));
 
@@ -52,12 +52,12 @@ void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
     //updating elements
     arma::mat A_old = A;
     
-    A(k,k) = A_old(k,k)*pow(cos,2) - 2*A_old(k,l)*cos*sin + 2*A_old(l,l)*pow(sin,2);
-    A(l,l) = A_old(l,l)*pow(cos,2) + 2*A_old(k,l)*cos*sin + 2*A_old(k,k)*pow(sin,2);
+    A(k,k) = A_old(k,k)*pow(cos,2) - 2*A_old(k,l)*cos*sin + A_old(l,l)*pow(sin,2);
+    A(l,l) = A_old(l,l)*pow(cos,2) + 2*A_old(k,l)*cos*sin + A_old(k,k)*pow(sin,2);
     A(k,l) = 0;
     A(l,k) = 0;
 
-    for(double i = 0; i < (A.n_cols-1); i++){
+    for(double i = 0; i < (A.n_cols); i++){
         if(i != k && i != l){
             A(i,k) = A_old(i,k)*cos - A_old(i,l)*sin;
             A(k,i) = A(i,k);
@@ -68,11 +68,10 @@ void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
 
     arma::mat R_old = R;
 
-    for(double i = 0; i < (R.n_cols-1); i++){
-        R(i,k) = R_old(i,k)*cos - R(i,l)*sin;
-        R(i,l) = R_old(i,l)*cos + R(i,k)*sin;
+    for(double i = 0; i < (R.n_cols); i++){
+        R(i,k) = R_old(i,k)*cos - R_old(i,l)*sin;
+        R(i,l) = R_old(i,l)*cos + R_old(i,k)*sin;
     }
-
     
 
 }
@@ -85,36 +84,55 @@ void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l){
 // - Stops if it the number of iterations reaches "maxiter"
 // - Writes the number of iterations to the integer "iterations"
 // - Sets the bool reference "converged" to true if convergence was reached before hitting maxiter
-void jacobi_eigensolver(arma::mat& A, double eps, arma::vec& eigenvalues, arma::mat& eigenvectors, 
+void jacobi_eigensolver(const arma::mat& A, double eps, arma::vec& eigenvalues, arma::mat& eigenvectors, 
                         const int maxiter, int& iterations, bool& converged){
     
     arma::mat R = arma::mat(A.n_rows, A.n_cols, arma::fill::eye);
-
+    arma::mat D = A;
     int k;
     int l;
-    double max_value = max_offdiag_symmetric(A, k, l);
-    //iterations = 1;
+    double max_value = max_offdiag_symmetric(D, k, l);
+    iterations = 0;
 
-    // while(max_value > eps){
-    //     jacobi_rotate(A, R, k, l);
-    //     max_value = max_offdiag_symmetric(A, k, l);
-    //     iterations += 1;
-    // }
+    //std::cout<< max_value;
+    //std::cout<< eps;
 
-    //jacobi_rotate1(A, R, k, l);
+    while(max_value > pow(10,-8)){
+        jacobi_rotate(D, R, k, l);
+        max_value = max_offdiag_symmetric(D, k, l);
+        std::cout<< l;
+        std::cout<< "#################################################################################################################################################";
+        std::cout<< k;
+        iterations += 1;
+    }
 
-    std::cout<< A;
+    //std::cout<< iterations; 
+    std::cout<< D;
+    // //std::cout<< A;
+    std::cout<< "#################################################################################################################################################";
     std::cout<< R;
-    
+
+    std::cout<< "#################################################################################################################################################";
+
+    arma::vec eigenvaltrue;
+    arma::mat eigenvectrue;
+    arma::eig_sym(eigenvaltrue, eigenvectrue, A);
+    std::cout << eigenvectrue;
+    std::cout << eigenvaltrue;
+
+    std::cout<< "#################################################################################";
+    std::cout<< A;
+
+
     }
 
     double max_offdiag_symmetric(const arma::mat& A, int& k, int& l){
 
     
     double max_value = 0;
-
-    for(int i=0; i < A.n_rows-1; i++){
-        for(int j=0;j < A.n_cols-1; j++){
+ 
+    for(int i=0; i < A.n_rows; i++){
+        for(int j=0;j < A.n_cols; j++){
 
             if(i!=j){
                 if(abs(A(i,j))>max_value){
---
+++ Task4_matrix.cpp
@@ -0,0 +1,30 @@
+
+
+#include <armadillo>
+#include <iostream>
+#include <math.h>
+
+
+int main(){
+
+    arma::mat T = arma::mat(6,6).fill(0.);
+    T.diag() += 1;
+
+    T(0,5) = 0.5;
+    T(5,0) = 0.5;
+
+    T(4,1) = -0.7;
+    T(3,2) = -0.7;
+   
+    T(2,3) = -0.7;
+    T(1,4) = -0.7;
+  
+
+    T.save("file4");
+
+    //int k;
+    //int l;
+
+    //double max_value = max_offdiag_symmetric(T, k, l);
+
+}
